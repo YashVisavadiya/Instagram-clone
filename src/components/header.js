ae81b8fc1ce23bf40adbc7d40037ba1e59ae1887
yashVisavadia <yashvishavadia6996@gmail.com>
2023-04-29 23:30:49 +0530

    Added Unknown user image

--- src/components/header.js
+++ src/components/header.js
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import FirebaseContext from '../context/firebase';
 import UserContext from '../context/user';
 import * as ROUTES from '../constants/routes';
+import { DEFAULT_IMAGE_PATH } from '../constants/paths';
 
 export default function Header() {
   const { firebase } = useContext(FirebaseContext);
   const { user } = useContext(UserContext);
 
   return (
-    <header className="h-16 bg-white border-b border-gray-primary mb-8">
+    <header className="h-16 bg-white border-b border-gray-primary mb-8 pt-2 pb-2">
       <div className="container mx-auto max-w-screen-lg h-full">
         <div className="flex justify-between w-full">
           <div className="text-gray-700 text-center flex items-center align-items cursor-pointer">
@@ -73,6 +74,9 @@ export default function Header() {
                       src={`/images/avatars/${user?.displayName}.jpg`}
                       className="rounded-full h-8 w-8 flex"
                       alt={`${user?.displayName} profile`}
+                      onError={(e) => {
+                        e.target.src = DEFAULT_IMAGE_PATH;
+                      }}
                     />
                   </Link>
                 </div>
